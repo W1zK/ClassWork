119c6f93786286c019aa2863fdb86e6b89ca4b77
w1zK <rezorqqq5@gmail.com>
2020-03-09 14:44:06 +0200

    First commit

---
+++ src/main/java/com/courses/management/common/Command.java
@@ -0,0 +1,13 @@
+package com.courses.management.common;
+
+public interface Command {
+    String command();
+    void process();
+
+    default boolean canProcess(String command){
+        return command.trim().equals(command());
+
+
+
+    }
+}
